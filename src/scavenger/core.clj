b462d9283b38f9dcadc762249f96145f255ce342
Frode Danielsen <frode@e5r.no>
2015-11-21 18:48:38 +0100

    Fix basic CORS support
    Since our client side app and backend API is currently run under
    different ports, XHR from the client app to the backend are cross origin
    requests. Thus we need to support the preflight OPTIONS request from the
    client and respond with appropriate CORS headers as well as allow origin
    on the actual GET request.
    
    Further abstraction of this should definitely be done, as well as
    complete the support for POST requests.

--- src/scavenger/core.clj
+++ src/scavenger/core.clj
@@ -2,7 +2,7 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [ring.util.response :refer [response]])
+            [ring.util.response :refer [response header]])
   (:use [datomic.api :only [db q] :as d]))
 
 (def uri "datomic:free://localhost:4334/items")
@@ -13,14 +13,21 @@
   (map first (q '[:find (pull ?c [*]) :where [?c item/name]] (db conn))))
 
 (defroutes app-routes
+  (OPTIONS "/items" []
+    (-> (response "")
+      (header "Access-Control-Allow-Methods" "GET")
+      (header "Access-Control-Allow-Headers" "content-type")
+      (header "Access-Control-Allow-Origin" "*")))
   (GET "/items" []
-    (response (str (into [] (get-all-items)))))
+    (-> (response (str (into [] (get-all-items))))
+      (header "Access-Control-Allow-Origin" "*")))
   (POST "/items" {body :body}
     (let [tempid (d/tempid :items)
           data (merge (read-string (slurp body)) {:db/id tempid})
           tx @(d/transact conn [data])
           id (d/resolve-tempid (db conn) (:tempids tx) tempid)]
-      (response (str (d/touch (d/entity (db conn) id))))))
+      (-> (response (str (d/touch (d/entity (db conn) id))))
+        (header "Access-Control-Allow-Origin" "*"))))
   (route/not-found "Page not found"))
 
 (def app
