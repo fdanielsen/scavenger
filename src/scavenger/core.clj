5082c2228ed66fbaba5d6028cee41570221d46e4
Simen Heggestøyl <simen@e5r.no>
2015-11-22 13:55:00 +0100

    Change schema to include sorts and observations
    Change the schema to describe sorts and observations. Change the API and
    initial data to accommodate the change.

--- src/scavenger/core.clj
+++ src/scavenger/core.clj
@@ -9,23 +9,31 @@
 
 (def conn (d/connect uri))
 
-(defn get-all-items []
-  (map first (q '[:find (pull ?c [*]) :where [?c item/name]] (db conn))))
+(defn get-sorts []
+  (map first (q '[:find (pull ?c [*]) :where [?c sort/name]] (db conn))))
 
-(defn generate-response [data & [status]]
+(defn get-observations []
+  (map first (q '[:find (pull ?c [*]) :where [?c observation/sort]] (db conn))))
+
+(defn add-observation [edn-data]
+  (let [tempid (d/tempid :observations)
+        tx @(d/transact conn [(merge {:db/id tempid} edn-data)])
+        id (d/resolve-tempid (db conn) (:tempids tx) tempid)]
+    (d/touch (d/entity (db conn) id))))
+
+(defn gen-response [data & [status]]
   {:status (or status 200)
    :headers {"Content-Type" "application/edn"}
    :body (pr-str data)})
 
 (defroutes app-routes
-  (GET "/items" []
-    (generate-response (into [] (get-all-items))))
-  (POST "/items" {body :body}
-    (let [tempid (d/tempid :items)
-          data (merge (read-string (slurp body)) {:db/id tempid})
-          tx @(d/transact conn [data])
-          id (d/resolve-tempid (db conn) (:tempids tx) tempid)]
-      (generate-response (d/touch (d/entity (db conn) id)))))
+  (GET "/sorts" []
+    (gen-response (vec (get-sorts))))
+  (GET "/observations" []
+    (gen-response (vec (get-observations))))
+  (POST "/observations" {body :body}
+    (gen-response
+     (vec (add-observation (read-string (slurp body))))))
   (GET "/" []
     (-> (resource-response "index.html" {:root "public"})
         (content-type "text/html")))
